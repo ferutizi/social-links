59dc1fa93e6714492a953e375f071470146f276e
ferutizi <fer.utizi@gmail.com>
2024-03-22 17:51:51 -0300

    customized links, added personal style

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -5,8 +5,13 @@ interface ButtonProps {
 
 export default function Button({ url, name }: ButtonProps) {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-      <button type="button" className="bg-zinc-800">{name}</button>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ textDecoration: 'none' }}
+    >
+      <button type="button" className="w-full rounded-lg shadow-md py-2 text-sm bg-zinc-800 hover:text-orange-400 transition-all ease-in-out duration-200">{name}</button>
     </a>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Button from "./components/Button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <article>
+    <main className="flex h-screen flex-col items-center justify-center p-24">
+      <article className="flex flex-col w-80 p-6 rounded-lg bg-zinc-900 items-center">
         <img></img>
-        <h1>Fernando Utizi</h1>
-        <h2>Frontend Developer</h2>
-        <div className="flex flex-col gap-4 bg-zinc-900">
+        <h1 className="my-1">Fernando Utizi</h1>
+        <h2 className="mb-6 text-sm text-orange-400">Frontend Developer</h2>
+        <div className="flex flex-col gap-4 w-full">
           <Button url="https://www.linkedin.com/in/fernando-utizi-2a72a3233/" name="Linkedin" ></Button>
-          <Button url="https://ferutizi.github.io/Portfolio/" name="Portfolio" ></Button>
+          <Button url="https://ferutizi.github.io/Portfolio/" name="Portfolio"></Button>
           <Button url="https://www.linkedin.com/in/fernando-utizi-2a72a3233/" name="Online CV" ></Button>
           <Button url="https://github.com/ferutizi" name="Github" ></Button>
         </div>
