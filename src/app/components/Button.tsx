a71dcccf2cfddb42bf71e4853d57661c58a4948f
ferutizi <fer.utizi@gmail.com>
2024-03-23 18:11:06 -0300

    hover style

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -11,7 +11,7 @@ export default function Button({ url, name }: ButtonProps) {
       rel="noopener noreferrer"
       style={{ textDecoration: 'none' }}
     >
-      <button type="button" className="w-full rounded-lg shadow-md py-2 text-sm bg-zinc-800 hover:text-orange-400 transition-all ease-in-out duration-200">{name}</button>
+      <button type="button" className="w-full rounded-lg shadow-md py-2 text-sm bg-zinc-800 hover:text-orange-400 hover:border-orange-400 border-transparent border-b transition-all ease-in-out duration-200">{name}</button>
     </a>
   )
 }
